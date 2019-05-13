055e22cb9fa92ef5e4874b4993e5a321f0b6e7e4
harmyderoman <romangarmider@gmail.com>
2019-05-13 12:59:44 +0300

    toolbar and menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,88 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app>
+      <v-navigation-drawer 
+      app 
+      temporary
+      v-model="drawer"
+      >
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title>John Leider</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-tile 
+          v-for="link of links" 
+          :to="link.url"
+          :key="link.title">
+            <v-list-tile-action>
+              <v-icon icon left>{{ link.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-text="link.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+      </v-navigation-drawer>
+      
+       <v-toolbar dark color="primary">
+        <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+        ></v-toolbar-side-icon>
+
+        <v-toolbar-title class="white--text">Vue Blog</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+          
+          <v-btn 
+          flat
+          v-for="link in links"
+          :key="link.title">   
+            {{ link.title}}
+          <v-icon icon right>{{ link.icon}}</v-icon>
+         </v-btn>
+
+        </v-toolbar-items>
+
+      </v-toolbar>
+
+      <v-content>
+          <router-view></router-view>
+      </v-content>
+
+      <v-footer app></v-footer>
+
+    </v-app>  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: 'My Blog', icon: 'star', url: '/myblog'},
+        { title: 'New Post', icon: 'note_add', url: '/newpost'},
+        { title: 'Log In', icon: 'person', url: '/login'},
+        { title: 'Sign Up', icon: 'person_add', url:  '/signup' }
+      ]
+    }
+  }
+}
+</script>
